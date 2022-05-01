81d9d286762ed111b80117ca6202328180f3c5c7
Trevon Mays <mays.trevon94@gmail.com>
2022-05-01 16:35:30 -0700

    commit tree 5fc9242d6fe898b19635071d4851e862daa3f2bc

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(names, string){
+let results = []
+for (let name of names){
+    console.log(name)
+    results.push(`Thank you, ${name}, for the wonderful ${string} gift!`)
+}
+return results
+}
+function countDown(number){
+while (number > 0){
+    console.log(number)
+number-=1
+}
+console.log(number)
+}
+
+
+
+
+
+// let countup = 0;
+// while (countup < 10) {
+//   console.log(countup++);
+// }
